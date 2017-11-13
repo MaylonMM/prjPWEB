f4446c5e82d858d78586699c0dfa8374c701a0c4
dell <dell@192.168.99.133>
2017-11-13 12:58:45 -0200

    Inclusão da Tela Inicial

---
+++ prjPWEBHeitorBrunoMaylon/src/main/java/fatec/pweb/beans/locale/LocaleBeans.java
@@ -0,0 +1,27 @@
+package fatec.pweb.beans.locale;
+
+import java.util.Locale;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LocaleBeans {
+	private Locale locale = Locale.ENGLISH;
+  
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+}
